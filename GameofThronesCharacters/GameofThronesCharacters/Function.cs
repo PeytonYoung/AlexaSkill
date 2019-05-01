d70aac96c115aa2153c5aad4a69d7b52ddff5966
PeytonYoung <46727410+PeytonYoung@users.noreply.github.com>
2019-04-30 23:53:23 -0500

    fixed the errors

--- GameofThronesCharacters/GameofThronesCharacters/Function.cs
+++ GameofThronesCharacters/GameofThronesCharacters/Function.cs
@@ -8,7 +8,6 @@ using Alexa.NET.Request.Type;
 using Alexa.NET.Response;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json;
-using System.IO;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -20,7 +19,7 @@ namespace GameofThronesCharacters
 
         private static HttpClient httpClient;
 
-        public function()
+        public Function()
         {
             httpClient = new HttpClient();
         }
@@ -39,7 +38,7 @@ namespace GameofThronesCharacters
             return input?.ToUpper();
         }
 
-        public async Task<SKillResponse> FunctionHandler(SkillRequest input, ILambdaContext context)
+        public async Task<SkillResponse> FunctionHandler(SkillRequest input, ILambdaContext context)
         {
             var requestType = input.GetRequestType();
             string outputText = "";
@@ -65,47 +64,49 @@ namespace GameofThronesCharacters
                     {
                         return BodyResponse("I have no idea who you are talking about, you should try to say the first name someone i know", false);
                     }
-                    
-                    else if (intent.Intent.Name.Equals("AMAZON.StopIntent"))
-
-                    {
-
-                        return BodyResponse("You have now exited the Thrones Universe", true);
-
-                    }
-
-
-                    else 
-
-                    {
-
-                        return BodyResponse("Am confused. Please try again.", false);
-
-                    }
 
                     var characterInfo = await GetCharacterInfo(characterRequestedName, context);
                     {
-                         outputText = "This is " + characterInfo.name + "of house " + characterInfo.allegiances;
+                        outputText = "This is " + characterInfo.name + "of house " + characterInfo.allegiances;
                     }
 
-                    return BodyResponse(outputText, true);
+
+                }
+
+                else if (intent.Intent.Name.Equals("AMAZON.StopIntent"))
+
+                {
+
+                    return BodyResponse("You have now exited the Thrones Universe", true);
+
+                }
 
 
+                else
+
+                {
+
+                    return BodyResponse("Am confused. Please try again.", false);
+
+                }
             }
-            }
+
+            return BodyResponse(outputText, true);
+
+
         }
 
         private SkillResponse BodyResponse(string outputSpeech, bool shouldEndSession, string repromptText = "Just say, tell me about Jon Snow. To exit, say, exit.")
         {
             var response = new ResponseBody
             {
-                shouldEndSession = ShouldEndSession,
-                OutputSpeech = new PlainTextOutputSpeech { TextReader = outputSpeech }
+                ShouldEndSession = shouldEndSession,
+                OutputSpeech = new PlainTextOutputSpeech { Text = outputSpeech }
             };
 
-            if (repromtText != null)
+            if (repromptText != null)
             {
-                response.Repromt = new Reprompt() { outputSpeech = new PlainTextOutputSpeech() { TextReader = repromptText } };
+                response.Reprompt = new Reprompt() { OutputSpeech = new PlainTextOutputSpeech() { Text = repromptText } };
             }
 
             var skillResponse = new SkillResponse
@@ -116,7 +117,7 @@ namespace GameofThronesCharacters
             return skillResponse;
         }
 
-        private async Task<Character> GetCharacterInfo(string name)
+        private async Task<Character> GetCharacterInfo(string name, ILambdaContext context)
         {
             Character character = new Character();
 
@@ -126,16 +127,16 @@ namespace GameofThronesCharacters
             {
                 var response = await httpClient.GetStringAsync(uri);
                 context.Logger.LogLine($"Response from URL:\n{response}");
-                character = JsonConvert.DeserializeObject<List<Character>>(response).FirstorDefault();
+                character = JsonConvert.DeserializeObject<List<Character>>(response).FirstOrDefault();
             }
 
             catch (Exception ex)
 
             {
 
-                Context.Logger.LogLine($"\nException: {ex.Message}");
+                context.Logger.LogLine($"\nException: {ex.Message}");
 
-                Context.Logger.LogLine($"\nStack Trace: {ex.StackTrace}");
+                context.Logger.LogLine($"\nStack Trace: {ex.StackTrace}");
 
             }
 
